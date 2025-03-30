73877eb8bf2c83a27a88651a25fedd05d4ed528e
AlexVakhovski <ocdev.pro@gmail.com>
2025-03-30 03:57:07 -0400

    Fix db seeder Core

--- Core/src/install.php
+++ Core/src/install.php
@@ -7,9 +7,12 @@
  * @license GPLv3
  */
 
-$this->db->query("CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "aw_module_config` (
-        `id` BIGINT(11) NOT NULL,
+$this->db->query("
+    CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "aw_module_config` (
+        `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
         `code` VARCHAR(255) NOT NULL,
         `config` JSON NOT NULL,
-        PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `code_unique` (`code`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+");
