2d958a926a221be8d21ab324dd85d9ae1d06ba1f
AlexVakhovski <ocdev.pro@gmail.com>
2026-02-13 08:22:59 -0500

    Add Ukr language to Age Verification module

---
+++ age-verification/src/upload/admin/language/uk-ua/extension/module/aw_age_verification.php
@@ -0,0 +1,42 @@
+<?php
+
+// Heading
+$_['heading_title'] = '<span style="color:#0937cc;">alexwaha.com</span> - <i class="fa fa-puzzle-piece"></i> Підтвердження віку';
+$_['heading_main_title'] = 'alexwaha.com - Підтвердження віку';
+
+// Text
+$_['text_extension'] = 'Розширення';
+$_['text_success'] = 'Успішно: Ви змінили модуль підтвердження віку!';
+$_['text_edit'] = 'Редагувати модуль підтвердження віку';
+$_['text_enabled'] = 'Увімкнено';
+$_['text_disabled'] = 'Вимкнено';
+$_['text_home'] = 'Головна';
+$_['text_aw_support'] = '<div class="panel panel-success"><div class="panel-heading"><h3 class="panel-title"><i class="fa fa-life-ring"></i> Підтримка</h3></div><div class="panel-body"><p>Якщо у вас виникли питання щодо роботи модуля, звертайтесь до розробника:</p><ul><li><strong>Електронна пошта:</strong> <a href="mailto:support@alexwaha.com">support@alexwaha.com</a></li><li><strong>Офіційний сайт:</strong> <a href="https://alexwaha.com" target="_blank">alexwaha.com</a></li></ul></div></div>';
+
+// Entry
+$_['entry_status'] = 'Статус';
+$_['entry_title'] = 'Заголовок блоку';
+$_['entry_description'] = 'Опис';
+$_['entry_cookie_days'] = 'Термін зберігання відповіді (дні)';
+$_['entry_redirect_url'] = 'URL перенаправлення';
+
+// Tabs
+$_['tab_settings'] = 'Налаштування';
+$_['tab_support'] = 'Служба підтримки';
+
+// Help
+$_['help_title'] = 'Заголовок спливаючого вікна підтвердження віку';
+$_['help_description'] = 'Текст повідомлення для користувачів';
+$_['help_cookie_days'] = 'Кількість днів для зберігання відповіді користувача (за замовчуванням 30 днів)';
+$_['help_redirect_url'] = 'URL для перенаправлення користувачів, які відхилили підтвердження віку';
+
+// Button
+$_['button_save'] = 'Зберегти';
+$_['button_cancel'] = 'Скасувати';
+
+// Error
+$_['error_permission'] = 'Увага: У вас немає прав для зміни модуля підтвердження віку!';
+$_['error_title'] = 'Заголовок має бути від 3 до 255 символів!';
+$_['error_description'] = 'Опис має бути від 10 до 1000 символів!';
+$_['error_cookie_days'] = 'Термін зберігання має бути від 1 до 365 днів!';
+$_['error_warning'] = 'Увага: Будь ласка, уважно перевірте форму на наявність помилок!';
